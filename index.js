fc03a6c04809f1ab566e82873b8401744d0e9f6e
sapphira abayomi <saphieabayomi@gmail.com>
2018-11-19 14:21:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,10 +19,19 @@ function logWhisper(string){
 }
 
 
-sayHiToGrandma(string){
+function sayHiToGrandma(string){
 
   if(string.toLowerCase() === string) {
     return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string){
+
+    return "YES INDEED!"
+  }
+  if ( string === " I love you, Grandma."){
+
+      return "I love you, too."
   }
 
 }
