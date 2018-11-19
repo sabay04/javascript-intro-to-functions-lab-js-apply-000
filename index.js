cdce804e3446a726f887727bdf43da54939523b1
sapphira abayomi <saphieabayomi@gmail.com>
2018-11-19 14:16:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,13 +24,5 @@ sayHiToGrandma(string){
   if(string.toLowerCase() === string) {
     return "I can't hear you!"
   }
-  if (string.toUpperCase() === string){
-
-    return "YES INDEED!"
-  }
-  if ( string === " I love you, Grandma."){
-
-      return "I love you, too."
-  }
 
 }
